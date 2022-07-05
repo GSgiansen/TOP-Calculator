b552be47b60de9a80f89dc6fbf5b9e79ee475580
GianSen <giansenrockz@gmail.com>
2022-07-05 17:06:25 +0800

    Created raw outline of files

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # TOP-Calculator
+Attempt at the TOP calculator which simulates a physical calculator
---
+++ calculator.js
@@ -0,0 +1,10 @@
+const container=document.querySelector("#container");
+
+const display= document.createElement("div")
+display.textContent="DISPLAY"
+
+const numberpad=document.createElement("div")
+
+
+container.appendChild(display)
+container.appendChild(numberpad)
