949d9fec305b1b7114ff48e7f1f817182d3be2fb
GianSen <giansenrockz@gmail.com>
2022-07-06 15:34:25 +0800

    Included a modulo and backspace button

--- calculator.js
+++ calculator.js
@@ -12,6 +12,8 @@ container.appendChild(numberpad)
 let stack=[]
 let num=""
 
+let state=[stack,num]
+
 const operators =new Map([
     ["+",function add(a,b){
         return a+b;
@@ -24,6 +26,9 @@ const operators =new Map([
     }],
     ["/",function divide(a,b){
         return a/b;
+    }],
+    ["%",function mod(a,b){
+        return a%b;
     }]
 
 ])
@@ -57,16 +62,12 @@ function operate(){
 }
 
 function addToDisplay(){
+    let t=num
+    let tstack=Array.from(stack)
+    state=[tstack,t]
     if (!displayflag){
         display.textContent=""
         displayflag=true}
-    if (this.classList.contains("op")){
-        console.log(num)
-        stack.push(num)
-        num=""
-        deciflag=false;
-        stack.push(this.textContent)
-    }
     if (this.classList.contains("deci")){
         if(!deciflag){
             deciflag=true;
@@ -75,23 +76,57 @@ function addToDisplay(){
             return 
         }
     }
+    if (this.classList.contains("op")){
+        console.log(num)
+        stack.push(num)
+        num=""
+        deciflag=false;
+        stack.push(this.textContent)
+    }
+
     curr=display.textContent
     if (!this.classList.contains("op")) num+=this.textContent
     curr=curr+this.textContent
     display.textContent=curr
     console.log(stack)
+    
 }
 
+function back(){
+    stack=state[0]
+    num=state[1]
+    a=display.textContent.slice(0,-1)
+    display.textContent=a
+}
+
+const numbers=document.createElement("div")
 displayflag=false;
 deciflag=false;
-for(let i=0;i<10;i++){
+let i=1
+while (i<10){
+    const line=document.createElement("div")
+    for (let j=i;j<i+3;j++){
     const button=document.createElement("button")
-    button.textContent=i
+    button.textContent=j
     button.addEventListener("click",addToDisplay)
-    numberpad.appendChild(button)
-
-
+    line.appendChild(button)
+    }
+    numbers.appendChild(line)
+    i=i+3
 }
+const zerorow=document.createElement("div")
+const zero=document.createElement("button")
+zero.textContent=0
+zero.classList.add("zero")
+zero.addEventListener("click",addToDisplay)
+
+zerorow.appendChild(zero)
+const deci=document.createElement("button")
+deci.textContent="."
+deci.classList.add("deci")
+deci.addEventListener("click",addToDisplay)
+
+
 
 
 
@@ -99,7 +134,6 @@ const addB=document.createElement("button")
 addB.classList.add("op")
 addB.textContent="+"
 addB.addEventListener("click",addToDisplay)
-addB.value="+"
 const subtractB=document.createElement("button")
 subtractB.textContent="-"
 subtractB.classList.add("op")
@@ -119,25 +153,49 @@ divideB.addEventListener("click",addToDisplay)
 
 const equal=document.createElement("button")
 equal.textContent="="
+equal.classList.add("op")
 equal.addEventListener("click",operate)
 
 const wipe=document.createElement("button")
-wipe.textContent="CLEAR"
+wipe.textContent="AC"
+wipe.classList.add("op")
 wipe.addEventListener("click",clear)
 
-const deci=document.createElement("button")
-deci.textContent="."
-deci.classList.add("deci")
-deci.addEventListener("click",addToDisplay)
+const mod=document.createElement("button")
+mod.textContent="%"
+mod.classList.add("op")
+mod.addEventListener("click",addToDisplay)
 
-numberpad.appendChild(addB)
-numberpad.appendChild(subtractB)
-numberpad.appendChild(multiplyB)
-numberpad.appendChild(divideB)
-numberpad.appendChild(deci)
-numberpad.appendChild(equal)
+const backspace=document.createElement("button")
+backspace.textContent="C"
+backspace.classList.add("op")
+backspace.addEventListener("click",back)
 
-numberpad.appendChild(wipe)
+
+
+
+const ops1=document.createElement("div")
+ops1.classList.add("ops1")
+const ops2=document.createElement("div")
+ops2.classList.add("ops2")
+
+ops1.appendChild(addB)
+ops1.appendChild(subtractB)
+ops1.appendChild(multiplyB)
+ops1.appendChild(divideB)
+
+ops2.appendChild(backspace)
+ops2.appendChild(mod)
+ops2.appendChild(equal)
+ops2.appendChild(wipe)
+
+zerorow.appendChild(deci)
+numbers.appendChild(zerorow)
+numberpad.appendChild(numbers)
+
+numberpad.appendChild(ops1)
+
+numberpad.appendChild(ops2)
 
 
 
